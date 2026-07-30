3f3a4e5ef120b7ac7390130f8c8152f528152b95
DVDan-1969 <dvdudas@yahoo.com>
2026-07-30 20:16:39 +0300

    Add JPA repositories.

---
+++ src/main/java/org/example/ecomercestore/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package org.example.ecomercestore.repository;
+
+import org.example.ecomercestore.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
