1db609e3d3ca8185830c20c35640c33baaa95c81
DVDan-1969 <dvdudas@yahoo.com>
2026-08-06 20:46:29 +0300

    ProductService to use DTO and ProductMapper

--- src/main/java/org/example/ecomercestore/service/ProductServiceImpl.java
+++ src/main/java/org/example/ecomercestore/service/ProductServiceImpl.java
@@ -1,7 +1,12 @@
 package org.example.ecomercestore.service;
 
 import jakarta.transaction.Transactional;
+import org.example.ecomercestore.dto.ProductRequestDTO;
+import org.example.ecomercestore.dto.ProductResponseDTO;
+import org.example.ecomercestore.mapper.ProductMapper;
+import org.example.ecomercestore.model.Category;
 import org.example.ecomercestore.model.Product;
+import org.example.ecomercestore.repository.CategoryRepository;
 import org.example.ecomercestore.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,29 +17,58 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository repository;
+    private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
-    public ProductServiceImpl(ProductRepository repository) {
+    public ProductServiceImpl(ProductRepository repository,
+                              ProductMapper productMapper,
+                              CategoryRepository categoryRepository) {
         this.repository = repository;
+        this.productMapper = productMapper;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public List<Product> getAllProducts() {
-        return repository.findAll();
+    public List<ProductResponseDTO> getAllProducts() {
+
+        return repository.findAll()
+                .stream()
+                .map(productMapper::toResponseDTO)
+                .toList();
+
     }
 
     @Override
-    public Product getProductById(Long id) {
-        return repository.findById(id).orElse(null);
+    public ProductResponseDTO getProductById(Long id) {
+        Product product = repository.findById(id)
+                .orElseThrow(()->new RuntimeException("product not found"));
+
+        return productMapper.toResponseDTO(product);
     }
 
     @Override
-    public Product save(Product product) {
-        return repository.save(product);
+    public ProductResponseDTO save(ProductRequestDTO dto) {
+        Category category= categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(()->new RuntimeException("Category not found"));
+        Product product = productMapper.toEntity(dto,category);
+        Product savedProduct = repository.save(product);
+        return productMapper.toResponseDTO(savedProduct);
     }
 
     @Override
-    public Product update(Long id, Product product) {
-        return repository.save(product);
+    public ProductResponseDTO update(Long id, ProductRequestDTO dto) {
+        Product product = repository.findById(id)
+                .orElseThrow(()->new RuntimeException("Product not found"));
+        Category category=categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(()->new RuntimeException("Category not found"));
+        product.setName(dto.getProductName());
+        product.setDescription(dto.getProductDescription());
+        product.setPrice(dto.getProductPrice());
+        product.setQuantity(dto.getProductQuantity());
+        product.setImage(dto.getProductImage());
+        product.setCategory(category);
+        Product updatedProduct = repository.save(product);
+        return productMapper.toResponseDTO(updatedProduct);
     }
 
     @Override
